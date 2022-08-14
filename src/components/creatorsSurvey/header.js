74e947398e2fb54228714cdd1c39059593a7edf8
MuhammadmaazAbdulhameed <muhammadmaaz2018@gmail.com>
2022-08-14 08:51:39 -0700

    header for brands , creators , and influancers

---
+++ src/components/creatorsSurvey/header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button, Col, Row } from 'reactstrap'
+
+const Header = () => {
+    return (
+        <Row className='my-3'>
+            <Col lg='12' className='d-flex justify-content-between align-items-center'>
+                <div>
+                    <h3 className='text-black fw-bolder'>Creators Survey</h3>
+                </div>
+                <div>
+                    <Button.Ripple color='primary'>Add New</Button.Ripple>
+                </div>
+            </Col>
+        </Row>
+    )
+}
+
+export default Header
