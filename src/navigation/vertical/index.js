a754831db75e8818dd73c3bfc503c890c07c91fc
MuhammadmaazAbdulhameed <muhammadmaaz2018@gmail.com>
2022-08-14 08:53:29 -0700

    brands creators and Influencers  added to the sidebar

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -3,14 +3,26 @@ import { Mail, Grid } from "react-feather"
 export default [
   {
     id: "dashboard",
-    title: "dashboard",
+    title: "Dashboard",
     icon: <Grid size={20} />,
     navLink: "/dashboard"
   },
   {
-    id: "secondPage",
-    title: "Second Page",
+    id: "influencer",
+    title: "Influence's",
     icon: <Mail size={20} />,
-    navLink: "/second-page"
+    navLink: "/influencer"
+  },
+  {
+    id: "brands",
+    title: "Brands",
+    icon: <Mail size={20} />,
+    navLink: "/brands"
+  },
+  {
+    id: "creatorsSurvey",
+    title: "Creator's Survey",
+    icon: <Mail size={20} />,
+    navLink: "/creators-survey"
   }
 ]
