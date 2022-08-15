4cbe46612e9b14b53f7a51e13cbe252952366242
MuhammadmaazAbdulhameed <muhammadmaaz2018@gmail.com>
2022-08-15 15:31:19 -0700

    creators survey header

--- src/components/creatorsSurvey/header.js
+++ src/components/creatorsSurvey/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Col, Row } from 'reactstrap'
 
-const Header = () => {
+const Header = ({ modalVisible, modalVisibilityAction }) => {
     return (
         <Row className='my-3'>
             <Col lg='12' className='d-flex justify-content-between align-items-center'>
@@ -9,7 +9,7 @@ const Header = () => {
                     <h3 className='text-black fw-bolder'>Creators Survey</h3>
                 </div>
                 <div>
-                    <Button.Ripple color='primary'>Add New</Button.Ripple>
+                    <Button.Ripple color='primary' onClick={() => modalVisibilityAction(!modalVisible)}>Add New</Button.Ripple>
                 </div>
             </Col>
         </Row>
