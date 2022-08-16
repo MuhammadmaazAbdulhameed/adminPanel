baf87adf4b9c594a040e5f3a03688b19f8e00cd8
MuhammadmaazAbdulhameed <muhammadmaaz2018@gmail.com>
2022-08-16 02:58:04 -0700

    creators Survery updated

--- src/views/CreatorsSurvey/index.js
+++ src/views/CreatorsSurvey/index.js
@@ -1,39 +1,122 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import CreatorsSurveyModal from '../../components/creatorsSurvey/creatorsSurveyModal'
 import Header from '../../components/creatorsSurvey/header'
 import CreatorsSurveyTable from '../../components/creatorsSurvey/table'
+import { Failure, Success } from '../../components/toast'
 
 const CreatorsSurvey = () => {
     const [addCreatorsSurveyModal, setAddCreatorsSurveyModal] = useState(false)
-    const [question, setQuestion] = useState("")
-    const [answer_1, setAnswer_1] = useState("")
-    const [answer_2, setAnswer_2] = useState("")
-    const [answer_3, setAnswer_3] = useState("")
-    const [answer_4, setAnswer_4] = useState("")
-
-    const addUpdateCreatorsSurvey = () => {
-        console.log("first")
-        // const obj = {
-        //     question,
-        //     answer_1,
-        //     answer_2,
-        //     answer_3,
-        //     answer_4
-        // }
+    const [editModal, setEditModal] = useState(false)
+    const [creatorsSurveyObject, setCreatorsSurveyObject] = useState(
+        {
+            question: "",
+            answer_1: "",
+            answer_2: "",
+            answer_3: "",
+            answer_4: ""
+        })
+    const [dataList, setDataList] = useState([])
+    const getCreatorsSurvey = () => {
+        const token = localStorage.getItem("token")
+        axios.get(`http://authentic-web.authenticmatchinglimited.com/api/get_creator_surveys`,
+            {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then(response => {
+                setDataList(response?.data?.response?.detail)
+            })
+            .catch(err => {
+                Failure(err?.response?.data?.error?.message)
+            })
     }
+    const addUpdateCreatorsSurvey = () => {
+        const token = localStorage.getItem("token")
+        if (!editModal) {
+            axios.post(`http://authentic-web.authenticmatchinglimited.com/api/add_creator_survey`, creatorsSurveyObject,
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                .then(response => {
+                    getCreatorsSurvey()
+                    setCreatorsSurveyObject({
+                        question: "",
+                        answer_1: "",
+                        answer_2: "",
+                        answer_3: "",
+                        answer_4: ""
+                    })
+                    setAddCreatorsSurveyModal(false)
+                    setEditModal(false)
+                    Success(response?.data?.response?.message)
+                })
+                .catch(err => {
+                    Failure(err?.response?.data?.error?.message)
+                })
+        } else {
+            axios.patch(`http://authentic-web.authenticmatchinglimited.com/api/edit_creator_survey`, creatorsSurveyObject,
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                .then(response => {
+                    getCreatorsSurvey()
+                    setCreatorsSurveyObject({
+                        question: "",
+                        answer_1: "",
+                        answer_2: "",
+                        answer_3: "",
+                        answer_4: ""
+                    })
+                    setAddCreatorsSurveyModal(false)
+                    setEditModal(false)
+                    Success(response?.data?.response?.message)
+                })
+                .catch(err => {
+                    Failure(err?.response?.data?.error?.message)
+                })
+        }
+
+    }
+    useEffect(() => {
+        getCreatorsSurvey()
+    }, [])
     return (
         <div className=''>
-            <Header modalVisible={addCreatorsSurveyModal} modalVisibilityAction={setAddCreatorsSurveyModal} />
-            <CreatorsSurveyTable />
+            <Header
+                modalVisible={addCreatorsSurveyModal}
+                modalVisibilityAction={setAddCreatorsSurveyModal}
+                creatorsSurveyObject={creatorsSurveyObject}
+                setCreatorsSurveyObject={setCreatorsSurveyObject}
+                editModal={editModal}
+                setEditModal={setEditModal}
+            />
+            <CreatorsSurveyTable
+                data={dataList}
+                setData={setDataList}
+                modalVisible={addCreatorsSurveyModal}
+                modalVisibilityAction={setAddCreatorsSurveyModal}
+                creatorsSurveyObject={creatorsSurveyObject}
+                setCreatorsSurveyObject={setCreatorsSurveyObject}
+                editModal={editModal}
+                setEditModal={setEditModal}
+            />
             <CreatorsSurveyModal
                 modalVisible={addCreatorsSurveyModal}
                 modalVisibilityAction={setAddCreatorsSurveyModal}
-                question={question} setQuestion={setQuestion}
-                answer_1={answer_1} setAnswer_1={setAnswer_1}
-                answer_2={answer_2} setAnswer_2={setAnswer_2}
-                answer_3={answer_3} setAnswer_3={setAnswer_3}
-                answer_4={answer_4} setAnswer_4={setAnswer_4}
+                creatorsSurveyObject={creatorsSurveyObject}
+                setCreatorsSurveyObject={setCreatorsSurveyObject}
                 addUpdateFunction={addUpdateCreatorsSurvey}
+                editModal={editModal}
+                setEditModal={setEditModal}
             />
         </div>
     )
