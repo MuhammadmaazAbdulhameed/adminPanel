2cb320605aa687f0c4fac6a835e169035f704d4d
MuhammadmaazAbdulhameed <muhammadmaaz2018@gmail.com>
2022-08-16 13:32:35 -0700

    updated

---
+++ src/components/metrics/tikTokTable.js
@@ -0,0 +1,71 @@
+
+import { Trash2 } from 'react-feather'
+import { Button, Col, Row, Table } from 'reactstrap'
+
+const TikTokTable = ({ title, modalVisible, modalVisibilityAction, setKeepOnTrackObject, setEditModal }) => {
+    const modalToggleFunction = () => {
+        setEditModal(false)
+        setKeepOnTrackObject({
+            field: ""
+        })
+        modalVisibilityAction(!modalVisible)
+    }
+
+    return (
+        <>
+            <Row className='mb-1'>
+                <Col lg='12' className='d-flex justify-content-between align-items-center'>
+                    <div>
+                        <h3 className='text-black fw-bolder'>{title}</h3>
+                    </div>
+                    <div>
+                        <Button.Ripple color='primary' onClick={modalToggleFunction}>Add Field</Button.Ripple>
+                    </div>
+                </Col>
+            </Row>
+            <Table responsive bordered className='bg-white shadow-sm rounded'>
+                <thead >
+                    <tr>
+                        <th className='bg-primary text-white rounded-start border-0 text-center'>
+                            <small className='text-xs fw-lighter text-capitalize'>No</small>
+                        </th>
+                        <th className='bg-primary text-white border-0 '>
+                            <small className='text-xs fw-lighter text-capitalize'>
+                                name
+                            </small>
+                        </th>
+                        <th className='bg-primary text-white rounded-end border-0 text-center'>
+                            <small className='text-xs fw-lighter text-capitalize'>
+                                Action
+                            </small>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {[1, 2, 3, 4]?.map((item) => (
+                        <tr>
+                            <td className='border-bottom-0 border-start-0' align='center' width={10}>
+                                <small className='text-xs fw-lighter  text-secondary'>{item}</small>
+                            </td>
+                            <td className='border-bottom-0 border-start-0'  >
+                                <small className='text-xs fw-lighter  text-secondary'>Following</small>
+                            </td>
+                            <td className='border-bottom-0 border-start-0 border-end-0' align='center' width={"10%"}>
+                                <div className='d-flex justify-content-center gap-50'>
+                                    <Button.Ripple className='btn-icon py-50' color='danger'>
+                                        <Trash2 size={12} />
+                                    </Button.Ripple>
+                                    <Button.Ripple size='sm' color='primary'>
+                                        <small className='text-xs fw-lighter  '>{"Detail"}</small>
+                                    </Button.Ripple>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </>
+    )
+}
+
+export default TikTokTable
--- src/views/Campaigns/index.js
+++ src/views/Campaigns/index.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'reactstrap'
+import CampaignCategoriesModal from '../../components/campaigns/campaignCategoriesModal'
+import CampaignQuestionsModal from '../../components/campaigns/campaignQuestionsModal'
 import CampaignsCategoriesHeader from '../../components/campaigns/campaignsCategoriesHeader'
 import CampaignsCategoriesTable from '../../components/campaigns/campaignsCategoriesTable'
 import CampaignsQuestionHeader from '../../components/campaigns/campaignsQuestionHeader'
@@ -8,28 +11,150 @@ import CampaignsResultFormHeader from '../../components/campaigns/campaignsResul
 import Header from '../../components/campaigns/header'
 import InstagramTikTokTable from '../../components/campaigns/InstagramTikTokTable'
 import CampaignsTable from '../../components/campaigns/table'
+import { Failure, Success } from '../../components/toast'
 
 const Campaigns = () => {
+  const token = localStorage.getItem("token")
+
+  const [campaignQuestionsModal, setCampaignQuestionsModal] = useState(false)
+  const [editCampaignQuestionsModal, setEditCampaignQuestionsModal] = useState(true)
+  const [campaignQuestionsObject, setCampaignQuestionsObject] = useState({
+    question: "",
+    answer_1: "",
+    answer_2: "",
+    answer_3: "",
+    answer_4: ""
+  })
+
+  const [campaignCategory, setCampaignCategory] = useState([])
+  const [campaignCategoryModal, setCampaignCategoryModal] = useState(false)
+  const [editCampaignCategoryModal, setEditCampaignCategoryModal] = useState(false)
+  const [campaignCategoryObject, setCampaignCategoryObject] = useState({
+    name: ""
+  })
+
+  const getCategory = () => {
+    axios.get(`http://authentic-web.authenticmatchinglimited.com/api/get_categories`,
+      {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        setCampaignCategory(response?.data?.response?.detail)
+      })
+      .catch(err => {
+        Failure(err?.response?.data?.error?.message)
+      })
+  }
+  const addUpdateCampaignQuestionFunction = () => { }
+  const addUpdateCampaignCategoryFunction = () => {
+    if (!editCampaignCategoryModal) {
+      axios.post(`http://authentic-web.authenticmatchinglimited.com/api/add_category`,
+        campaignCategoryObject,
+        {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then(response => {
+          getCategory()
+          setCampaignCategoryObject({
+            name: ""
+          })
+          setCampaignCategoryModal(false)
+          setEditCampaignCategoryModal(false)
+          Success(response?.data?.response?.message)
+        })
+        .catch(err => {
+          Failure(err?.response?.data?.error?.message)
+        })
+    } else {
+      axios.patch(`http://authentic-web.authenticmatchinglimited.com/api/edit_category`,
+        campaignCategoryObject,
+        {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then(response => {
+          getCategory()
+          setCampaignCategoryObject({
+            name: ""
+          })
+          setCampaignCategoryModal(false)
+          setEditCampaignCategoryModal(false)
+          Success(response?.data?.response?.message)
+        })
+        .catch(err => {
+          Failure(err?.response?.data?.error?.message)
+        })
+    }
+  }
+  useEffect(() => {
+    getCategory()
+  }, [])
   return (
     <div>
       <Header />
       <CampaignsTable />
-      <CampaignsQuestionHeader />
+      <CampaignsQuestionHeader
+        modalVisible={campaignQuestionsModal}
+        modalVisibilityAction={setCampaignQuestionsModal}
+        editModal={editCampaignQuestionsModal}
+        setEditModal={setEditCampaignQuestionsModal}
+      />
       <CampaignsQuestionTable />
-      <CampaignsCategoriesHeader />
-      <CampaignsCategoriesTable />
+      <CampaignsCategoriesHeader
+        modalVisible={campaignCategoryModal}
+        modalVisibilityAction={setCampaignCategoryModal}
+        campaignCategoryObject={campaignCategoryObject}
+        setCampaignCategoryObject={setCampaignCategoryObject}
+        editModal={editCampaignCategoryModal}
+        setEditModal={setEditCampaignCategoryModal} />
+      <CampaignsCategoriesTable
+        data={campaignCategory}
+        setData={setCampaignCategory}
+        modalVisible={campaignCategoryModal}
+        modalVisibilityAction={setCampaignCategoryModal}
+        campaignCategoryObject={campaignCategoryObject}
+        setCampaignCategoryObject={setCampaignCategoryObject}
+        editModal={editCampaignCategoryModal}
+        setEditModal={setEditCampaignCategoryModal}
+      />
       <CampaignsResultFormHeader />
       <Row>
-        <Col>
+        <Col xs={6}>
           <InstagramTikTokTable title="Instagram" />
         </Col>
-        <Col>
+        <Col xs={6}>
           <InstagramTikTokTable title="TikTok" />
         </Col>
       </Row>
 
+      {/* Question Modal */}
+      <CampaignQuestionsModal
+        modalVisible={campaignQuestionsModal}
+        modalVisibilityAction={setCampaignQuestionsModal}
+        campaignQuestionsObject={campaignQuestionsObject}
+        setCampaignQuestionsObject={setCampaignQuestionsObject}
+        addUpdateFunction={addUpdateCampaignQuestionFunction}
+        editModal={editCampaignQuestionsModal}
+        setEditModal={setEditCampaignQuestionsModal}
+      />
+      {/* Categories Modal */}
+      <CampaignCategoriesModal
+        modalVisible={campaignCategoryModal}
+        modalVisibilityAction={setCampaignCategoryModal}
+        campaignCategoryObject={campaignCategoryObject}
+        setCampaignCategoryObject={setCampaignCategoryObject}
+        addUpdateFunction={addUpdateCampaignCategoryFunction}
+        editModal={editCampaignCategoryModal}
+        setEditModal={setEditCampaignCategoryModal} />
     </div>
   )
 }
-
 export default Campaigns 
