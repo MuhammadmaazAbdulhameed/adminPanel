ba7c416b1a3acb79afa3b0b67ee3594efdbd1b01
MuhammadmaazAbdulhameed <muhammadmaaz2018@gmail.com>
2022-08-20 09:31:58 -0700

    delete keep on track get keep on track API implemented

--- src/views/Metrics/index.js
+++ src/views/Metrics/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'reactstrap'
 import InstagramModal from '../../components/metrics/instagramModal'
 import InstagramTable from '../../components/metrics/instagramTable'
@@ -8,21 +8,47 @@ import MetricsHeader from '../../components/metrics/MetricsHeader'
 import TikTokModal from '../../components/metrics/tikTokModal'
 import TikTokTable from '../../components/metrics/tikTokTable'
 import { Failure, Success } from '../../components/toast'
+import appConstant from '../../configs/constants'
+// ** Third Party Components
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
 
 const Metrics = () => {
     const token = localStorage.getItem("token")
-
+    const [instagramTableData, setInstagramTableData] = useState([])
     const [instagramModal, setInstagramModal] = useState(false)
     const [editInstagramModal, setEditInstagramModal] = useState(false)
     const [instagramObject, setInstagramObject] = useState({
         field: ""
     })
 
+    const [tikTokTableData, setTikTokTableData] = useState([])
     const [tikTokModal, setTikTokModal] = useState(false)
     const [editTokModal, setEditTikTokModal] = useState(false)
     const [tikTokObject, setTikTokObject] = useState({
         field: ""
     })
+
+    const getKeepOnTrack = () => {
+        axios.get(`https://authentic-web.authenticmatchinglimited.com/api/get_keep_on_tracks`,
+            {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then(response => {
+                const data = response?.data?.response?.detail
+                console.log(data, "data")
+                setInstagramTableData(data?.filter((item) => item?.type === appConstant?.instagram))
+                setTikTokTableData(data?.filter((item) => item?.type === appConstant?.tiktok))
+            })
+            .catch(err => {
+                Failure(err?.response?.data?.error?.message)
+            })
+    }
     const updateAddInstagramFunction = () => {
         const obj = { ...instagramObject, type: "instagram" }
         if (!editInstagramModal) {
@@ -35,7 +61,7 @@ const Metrics = () => {
                     }
                 })
                 .then(response => {
-                    // getCategory()
+                    getKeepOnTrack()
                     setInstagramObject({
                         field: ""
                     })
@@ -56,7 +82,7 @@ const Metrics = () => {
                     }
                 })
                 .then(response => {
-                    // getCategory()
+                    getKeepOnTrack()
                     setInstagramObject({
                         field: ""
                     })
@@ -81,7 +107,7 @@ const Metrics = () => {
                     }
                 })
                 .then(response => {
-                    // getCategory()
+                    getKeepOnTrack()
                     setTikTokObject({
                         field: ""
                     })
@@ -102,7 +128,7 @@ const Metrics = () => {
                     }
                 })
                 .then(response => {
-                    // getCategory()
+                    getKeepOnTrack()
                     setTikTokObject({
                         field: ""
                     })
@@ -115,6 +141,52 @@ const Metrics = () => {
                 })
         }
     }
+    const deleteKeepOnTrackFunction = (id) => {
+        const data = { keep_on_track_id: id }
+        return MySwal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            customClass: {
+                confirmButton: 'btn btn-primary',
+                cancelButton: 'btn btn-danger ms-1'
+            },
+            buttonsStyling: false
+        }).then(function (result) {
+            if (result.value) {
+                axios.delete(`https://authentic-web.authenticmatchinglimited.com/api/delete_keep_on_track`,
+                    {
+                        headers: {
+                            Accept: 'application/json',
+                            Authorization: `Bearer ${token}`
+                        },
+                        data
+
+                    })
+                    .then(response => {
+                        getKeepOnTrack()
+                        Success(response?.data?.response?.message)
+                    })
+                    .catch(err => {
+                        Failure(err?.response?.data?.error?.message)
+                    })
+                console.log(result.value, "result.value")
+                MySwal.fire({
+                    icon: 'success',
+                    title: 'Deleted!',
+                    text: 'Your file has been deleted.',
+                    customClass: {
+                        confirmButton: 'btn btn-success'
+                    }
+                })
+            }
+        })
+    }
+    useEffect(() => {
+        getKeepOnTrack()
+    }, [])
     return (
         <div>
             <MetricsHeader />
@@ -128,6 +200,9 @@ const Metrics = () => {
                         setKeepOnTrackObject={setInstagramObject}
                         editModal={editInstagramModal}
                         setEditModal={setEditInstagramModal}
+                        data={instagramTableData}
+                        deleteAction={deleteKeepOnTrackFunction}
+
                     />
                     <InstagramModal
                         modalVisible={instagramModal}
@@ -137,6 +212,7 @@ const Metrics = () => {
                         editModal={editInstagramModal}
                         setEditModal={setEditInstagramModal}
                         addUpdateFunction={updateAddInstagramFunction}
+
                     />
                 </Col>
                 <Col xs={6}>
@@ -147,6 +223,9 @@ const Metrics = () => {
                         setKeepOnTrackObject={setTikTokObject}
                         editModal={editTokModal}
                         setEditModal={setEditTikTokModal}
+                        data={tikTokTableData}
+                        deleteAction={deleteKeepOnTrackFunction}
+
                     />
                     <TikTokModal
                         modalVisible={tikTokModal}
