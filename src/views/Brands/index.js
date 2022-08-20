e39fb713d12757e663a487d47906e714926a4c3f
MuhammadmaazAbdulhameed <muhammadmaaz2018@gmail.com>
2022-08-20 09:32:39 -0700

    get brands API implemented

--- src/views/Brands/index.js
+++ src/views/Brands/index.js
@@ -1,10 +1,36 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import Header from "../../components/brands/header"
 import BrandsTable from "../../components/brands/table"
+
 const Brands = () => {
+    const token = localStorage.getItem("token")
+    const [dataList, setDataList] = useState([])
+    const getBrands = () => {
+        axios.get(`https://authentic-web.authenticmatchinglimited.com/api/get_influencers`,
+            {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then(response => {
+                setDataList(response?.data?.response?.detail)
+            })
+            .catch(err => {
+                Failure(err?.response?.data?.error?.message)
+            })
+    }
+
+    useEffect(() => {
+        getBrands()
+    }, [])
     return (
         <div className=''>
             <Header />
-            <BrandsTable />
+            <BrandsTable 
+            data={dataList}
+            setData={setDataList} />
         </div>
     )
 }
